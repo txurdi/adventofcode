b07685f83be8d7330381b7afedef9e3adc2f7f3c
txurdi <txurdi@gmail.com>
2025-12-08 15:00:21 +0100

    Preparing day 2

---
+++ src/Challenges/Year2025Day2Challenge.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Challenges;
+
+use App\util\fileDataHelper;
+
+class Year2025Day2Challenge extends YearDayChallenge
+{
+    public function __construct(
+        private readonly int $year,
+        private readonly int $day,
+        private readonly string $projectDir,
+        private readonly bool $debug
+    )
+    {
+        parent::__construct($year, $day, $projectDir, $debug);
+    }
+
+    public function execute(string $half, ?string $test='1'): string
+    {
+        $this->format = fileDataHelper::DATA_FORMAT_LINES;
+        return parent::execute($half, $test);
+    }
+
+    protected function executePart1(): void
+    {
+
+        $this->result = 0;
+    }
+
+    protected function executePart2(): void
+    {
+
+        $this->result = 0;
+    }
+}
