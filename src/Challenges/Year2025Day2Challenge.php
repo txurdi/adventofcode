48bf4c7dbf1d9c5d28a9c1d89e1a929da1ae687b
txurdi <txurdi@txurdi.net>
2025-12-11 19:26:37 +0100

    Passed day 2 half 1

--- src/Challenges/Year2025Day2Challenge.php
+++ src/Challenges/Year2025Day2Challenge.php
@@ -24,17 +24,38 @@ class Year2025Day2Challenge extends YearDayChallenge
     protected function executePart1(): void
     {
         $result = trim($this->dataStr);
+        $ranges = explode(",", $result);
 
-        var_dump($result);
-        exit();
-//        $result[$numLine] = preg_split("/[\s,]+/", (trim($linea)));
+        $allInvalidIds = [];
+        foreach ($ranges as $range) {
+            [$from, $to] = explode("-", $range);
+            $invalidIds = $this->getInvalidIds($from, $to);
+            $allInvalidIds = array_merge($allInvalidIds, $invalidIds);
+        }
 
-        $this->result = 0;
+        $this->result = array_sum($allInvalidIds);
     }
 
     protected function executePart2(): void
     {
 
         $this->result = 0;
+    }
+
+    private function getInvalidIds(string $from, string $to)
+    {
+        $invalidIds = [];
+        for ($i = (int)$from; $i <= (int)$to; $i++) {
+            $numCar = strlen((string)$i);
+            if ($numCar % 2 == 1) continue;
+            $mitad = $numCar / 2;
+            $inicio = substr($i, 0, $mitad);
+            $final = substr($i, $mitad);
+            if ($inicio == $final) {
+                $invalidIds[] = $i;
+            }
+        }
+
+        return $invalidIds;
     }
 }
